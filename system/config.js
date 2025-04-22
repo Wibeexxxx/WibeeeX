2a87bd290eed51985f26fbefbd2eb85c23ac0fb6
TOBI-12 <jesusgabrielp994@gmail.com>
2025-04-21 23:30:41 -0600

    iniciado

---
+++ system/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ RIKZZ BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: RikzzZhiro           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 1.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6283134463914"] 
+global.namabot = 'Lynex-v2'
+//======================
+global.mess = { 
+owner: 'Waduhh! ,Lu bukan owner gw bg🗣️',
+premium: 'Lah Lu siapa Njr, Bukan prem tai🐦',
+succes: 'Success Bang😾'
+}
+//======================
